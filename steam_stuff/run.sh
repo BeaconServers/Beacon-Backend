fcbbd4a13fbab0ff912822fc453c05298c162d4c
Susorodni <lgaston@myhoosierneighbors.com>
2021-01-10 16:21:56 -0500

    Minor Command Error
    Accidentally used +quit instead of +exit.
    
    Co-Authored-By: William Batista <45850508+billyb2@users.noreply.github.com>

--- steam_stuff/run.sh
+++ steam_stuff/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steam/csgo +app_update 740 +quit
+/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steam/csgo +app_update 740 +exit
 cd /home/steam/csgo/
 /home/steam/csgo/srcds_run -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2
