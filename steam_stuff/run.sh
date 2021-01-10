328bb2d48b10d1ca56b396df725f2c0ba17a72fc
Susorodni <lgaston@myhoosierneighbors.com>
2021-01-10 16:17:39 -0500

    Removed csgo install script file
    Ah Docker on Windows, beautiful and glorious. 'Cross platform', they said, 'work will be so easy', they said. Anyways, bc Docker on Windows is fucky with mounting files, I just run the steamcmd install script all in one line.
    
    - Written by William Batista.
    
    Co-Authored-By: William Batista <45850508+billyb2@users.noreply.github.com>

--- steam_stuff/run.sh
+++ steam_stuff/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/steamcmd/steamcmd.sh +runscript /home/steam/csgo_install.txt
+/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steam/csgo +app_update 740 +quit
 cd /home/steam/csgo/
 /home/steam/csgo/srcds_run -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2
