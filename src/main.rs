e140175582b4ab63ee20f3cc79b930a829700c79
William Batista <bootlegbilly@protonmail.ch>
2021-02-26 13:27:44 -0500

    Login and Register Authentication
    The server now verifies passwords using argon2id, which is a slower hashing algorithm then the previously used BCrypt in PHP, while still being much faster!

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,121 @@
-// If I ever want to combine threads with async
-//use async_std::task;
-use tokio::net::{TcpStream, TcpListener};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::spawn;
+use warp::{Filter, http::StatusCode};
 use tokio::runtime::Builder;
-use format::validate_request;
+use serde::Deserialize;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use std::collections::HashMap;
+use tokio::task;
+use argon2::{self, Config};
+use rand::RngCore;
 
-mod format;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
-    let rt =  Builder::new_multi_thread().build().unwrap();
-    
-    rt.block_on(async {
-        let listener = TcpListener::bind("127.0.0.1:7878").await?;
-
-        loop {
-            let (tcp_stream, _) = listener.accept().await?;
-            spawn(async move {
-            handle_connection(tcp_stream).await.unwrap();
-            
-            });
-        }
-    
-    })
-        
+#[derive(Debug, Deserialize)]
+struct User {
+    username: String,
+    password: String,
+    email: String,
 }
 
-async fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
-    let mut buffer: [u8; 1024] = [0; 1024];
-    stream.read(&mut buffer).await?;
+
+fn main() {
+let rt =  Builder::new_multi_thread().enable_io().build().unwrap();
+rt.block_on(async {
+    let db = Arc::new(Mutex::new(HashMap::<String, User>::new()));
+    let db = warp::any().map(move || Arc::clone(&db));
+
+    let register = warp::post()
+    .and(warp::path("register"))
+    .and(warp::body::form())
+    .and(db.clone())
+    .and_then(register);
     
-    let buffer = String::from_utf8_lossy(&buffer).to_string();
-    let (response, path, post_data, get_data) = validate_request(buffer).await;
+    let login = warp::post()
+    .and(warp::path("login"))
+    .and(warp::body::form())
+    .and(db.clone())
+    .and_then(login);
     
-    stream.write(response.as_bytes()).await?;
-    stream.flush().await?;
-    Ok(())
+    let routes = warp::post().and(
+        register
+        .or(login)
+    );
+
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 7878))
+        .await;
+
+})
+}
+
+
+
+async fn register(new_user: User, db: Arc<Mutex<HashMap<String, User>>>, ) -> Result<impl warp::Reply, warp::Rejection> {
+    let mut users = db.lock().await;
+        
+    if users.contains_key(&new_user.username) {
+        return Ok(
+        warp::reply::with_status(
+            "You've already signed up!",
+            StatusCode::BAD_REQUEST,
+        )
+        );
+    }
+    
+    let hashed_user = User {
+        username: new_user.username,
+        password: hash(new_user.password).await,
+        email: new_user.email,
+    };
+    
+    
+    println!("{:?}", hashed_user);
+    users.insert(hashed_user.username.clone(), hashed_user);
+    Ok(warp::reply::with_status(
+            "Registered sucessfuly!",
+            StatusCode::OK,
+        ))
+}
+
+async fn login(auth_user: User, db: Arc<Mutex<HashMap<String, User>>>) -> Result<impl warp::Reply, warp::Rejection> {
+    let users = db.lock().await;
+    
+    match users.get(&auth_user.username) {
+        None => Ok(warp::reply::with_status(
+                                "Bad username!",
+                                StatusCode::UNAUTHORIZED,
+                            )),
+        Some(user) => {
+            if verify((&*user.password).to_string(), auth_user.password).await {
+                Ok(warp::reply::with_status(
+                        "Logged in sucessfully!",
+                        StatusCode::OK,
+            ))
+            
+            } else {
+                Ok(warp::reply::with_status(
+                        "Bad password!",
+                        StatusCode::UNAUTHORIZED,
+                ))
+            }
+        }
+    }
+}
+
+async fn verify(passwd: String, auth_passwd: String) -> bool {
+    task::spawn_blocking(move || {
+        argon2::verify_encoded(&passwd, auth_passwd.as_bytes()).unwrap()
+        
+    }).await.unwrap()    
+}
+
+
+async fn hash(passwd: String) -> String {
+    // Run in tokio's spawn_blocking for efficiency
+    task::spawn_blocking(move || {
+        let mut salt = [0u8; 64];
+        rand::thread_rng().fill_bytes(&mut salt);
+    
+        argon2::hash_encoded(passwd.as_bytes(), &salt, &Config::default()).unwrap()
+        
+    }).await.unwrap()
 }
