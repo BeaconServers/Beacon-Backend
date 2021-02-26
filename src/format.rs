4f68aa4f910b73c4629342b46ebe3dc76c3a082e
William Batista <bootlegbilly@protonmail.ch>
2021-02-25 20:46:57 -0500

    Converted the format.rs file to use async
    We now have a very basic webserver! The format.rs file is ugly as sin at
    the moment, unfortuanetly, so I should work on that

--- src/format.rs
+++ src/format.rs
@@ -1,12 +1,18 @@
 #![forbid(unsafe_code)]
 use regex::Regex;
-use rust_embed::RustEmbed;
 use std::collections::HashMap;
+use reinda::{assets, Assets, Config, Setup};
 use lazy_static::lazy_static;
 
-#[derive(RustEmbed)]
-#[folder = "Web"]
-struct Asset;
+
+const ASSETS: Setup = assets! {
+    // Folder which contains your assets, relative to your `Cargo.toml`.
+    #![base_path = "Web"]
+
+    // List of assets to include, with different settings.
+    "index.html": { template },
+    "404.html": { template },
+};
 
 //All the regexes used to validate the HTTP header
 const GET_RE: &str = r"^GET";
@@ -22,75 +28,31 @@ const BAD_REQUEST_RESPONSE_RE_STR: &str =
     "400 Error, Bad Request!, screenshot this and send it to Will:";
 const BAD_REQUEST_HEADER_RE_STR: &str = "HTTP/1.1 400 BAD REQUEST Content-Length:";
 
+lazy_static! {
+    //Kind of verbose, but processes once and never again
+    static ref GET_COMBINED_PATH_RE: Regex =  Regex::new(&format!("({}) ({})", GET_RE, PATH_RE)).unwrap();
+    static ref POST_COMBINED_PATH_RE: Regex = Regex::new(&format!("({}) ({})", POST_RE, PATH_RE)).unwrap();
+
+    static ref GET_VALID_HTTP_REQUEST_HEADER_RE: Regex = Regex::new(&format!("({}) ({}) ({})", GET_RE, PATH_RE, HTTP_VER_RE)).unwrap_or(Regex::new("").unwrap());
+    static ref POST_VALID_HTTP_REQUEST_HEADER_RE: Regex = Regex::new(&format!("({}) ({}) ({})", POST_RE, PATH_RE, HTTP_VER_RE)).unwrap();
+    
+    static ref DATA_REGEX: Regex = Regex::new(DATA_RE).unwrap();
+}
+
 //I know making it always give back a tuple is a stupid solution, and slightly wasteful, but it
 // works so I probably won't be changing it for now.
 
 //The tuple is: (response_string, path, post req data, get req data)
-pub fn validate_request(buffer: String) -> (String, String, Option<String>, Option<String>) {
-    //Since bad response is used so often in the if tree, I made it into a closure
-    let bad_request = || -> String {
-        //Not using the format::response function since the response itself changes depending on the request
-        let page = format!("{}\n{}", BAD_REQUEST_RESPONSE_RE_STR, buffer);
-        format!(
-            "{} {:?}\r\n\r\n{}",
-            BAD_REQUEST_HEADER_RE_STR,
-            page.len(),
-            page
-        )
-    };
-
-    lazy_static! {
-        //Kind of verbose, but processes once and never again
-        static ref GET_COMBINED_PATH_RE: Regex =  Regex::new(&format!("({}) ({})", GET_RE, PATH_RE)).unwrap();
-        static ref POST_COMBINED_PATH_RE: Regex = Regex::new(&format!("({}) ({})", POST_RE, PATH_RE)).unwrap();
-
-        static ref GET_VALID_HTTP_REQUEST_HEADER_RE: Regex = Regex::new(&format!("({}) ({}) ({})", GET_RE, PATH_RE, HTTP_VER_RE)).unwrap_or(Regex::new("").unwrap());
-        static ref POST_VALID_HTTP_REQUEST_HEADER_RE: Regex = Regex::new(&format!("({}) ({}) ({})", POST_RE, PATH_RE, HTTP_VER_RE)).unwrap();
-
-    }
-
-    let mut path = "";
-
-    let mut request_regex_check = |request_type_re: &str| -> String {
-        let (combined_path_re, valid_http_request_header_re) = {
-            if request_type_re == GET_RE {
-                path = GET_COMBINED_PATH_RE
-                    .captures(&buffer)
-                    .unwrap()
-                    .get(2)
-                    .map_or("", |m| m.as_str());
-                (&*GET_COMBINED_PATH_RE, &*GET_VALID_HTTP_REQUEST_HEADER_RE)
-            } else {
-                path = POST_COMBINED_PATH_RE
-                    .captures(&buffer)
-                    .unwrap()
-                    .get(2)
-                    .map_or("", |m| m.as_str());
-                (&*POST_COMBINED_PATH_RE, &*POST_VALID_HTTP_REQUEST_HEADER_RE)
-            }
-        };
-
-        path = match path.find("?") {
-            Some(i) => &path[..i],
-            None => path,
-        };
-
-        if combined_path_re.is_match(&buffer) && valid_http_request_header_re.is_match(&buffer) {
-            format_response(
-                combined_path_re
-                    .captures(&buffer)
-                    .unwrap()
-                    .get(2)
-                    .map_or("", |m| m.as_str()),
-                200,
-            )
-        } else {
-            bad_request()
-        }
-    };
-
+pub async fn validate_request(buffer: String) -> (String, String, Option<String>, Option<String>) {            
     //This is wasteful, since it performs the Regex check twice
     return if Regex::new(GET_RE).unwrap().is_match(&buffer) {
+        let path =
+        GET_COMBINED_PATH_RE
+        .captures(&buffer)
+        .unwrap()
+        .get(2)
+        .map_or("", |m| m.as_str());
+    
         let request = GET_COMBINED_PATH_RE
             .captures(&buffer)
             .unwrap()
@@ -103,29 +65,32 @@ pub fn validate_request(buffer: String) -> (String, String, Option<String>, Opti
         };
 
         (
-            request_regex_check(GET_RE),
+            request_regex_check(&buffer, GET_RE).await,
             path.to_string(),
             None,
             get_data,
         )
     } else if Regex::new(POST_RE).unwrap().is_match(&buffer) {
-        lazy_static! {
-            static ref DATA_REGEX: Regex = Regex::new(DATA_RE).unwrap();
-        }
-        
+        let path =
+        POST_COMBINED_PATH_RE
+        .captures(&buffer)
+        .unwrap()
+        .get(2)
+        .map_or("", |m| m.as_str());
+    
         let data = DATA_REGEX
             .captures(&buffer)
             .unwrap()
             .get(0)
             .map_or("", |m| m.as_str());
 
-        (request_regex_check(POST_RE), path.to_string(), Some(data.to_string()), None,)
+        (request_regex_check(&buffer, POST_RE).await, path.to_string(), Some(data.to_string()), None,)
     } else {
-        (bad_request(), "/404.html".to_string(), None, None)
+        (bad_request_page(&buffer).await, "/404.html".to_string(), None, None)
     };
 }
 
-fn format_response(mut page: &str, mut response_code: u16) -> String {
+async fn format_response(mut page: &str, response_code: u16) -> Result<String, Box<dyn std::error::Error> > {
     //I decided to use a HashMap, in case this can be used later down the road
     //Only implemented a few response codes for simplicity
     lazy_static! {
@@ -146,29 +111,61 @@ fn format_response(mut page: &str, mut response_code: u16) -> String {
         Some(index) => &page[..index],
         None => page,
     };
-
-    let not_found_closure = || {
-        if page == "" {
-            Asset::get("index.html").unwrap()
-        } else {
-            response_code = 404;
-            Asset::get("404.html").unwrap()
-        }
+    
+    let assets = Assets::new(ASSETS, Config::default()).await?;
+    
+    let embedded_page_bytes = match assets.get(page).await? {
+        Some(b) => b,
+        None => {
+            if page == "" {
+                assets.get("index.html").await?.unwrap()
+            } else {
+                assets.get("404.html").await?.unwrap()
+            }
+        },
     };
+    
+    
+    let embedded_page_str = String::from_utf8_lossy(&embedded_page_bytes).to_string();
 
-    //Just get the page using rust_embed
-    let embedded_page = Asset::get(page).unwrap_or_else(not_found_closure);
-    //Then convert that page into a string
-    //The extra to_string is to solve returns a value referencing data owned by the current function
-    let embedded_page_str: String = std::str::from_utf8(embedded_page.as_ref())
-        .unwrap()
-        .to_string();
-
-    format!(
+    Ok(format!(
         "HTTP/1.1 {} {} Content-Length: {:?}\r\n\r\n{}",
         response_code,
-        RESPONSE_CODE_HASHMAP.get(&response_code).unwrap(),
+        RESPONSE_CODE_HASHMAP.await.get(&response_code).unwrap(),
         embedded_page_str.len(),
         embedded_page_str
+    ))
+}
+
+async fn bad_request_page(buffer: &String) -> String{
+    //Since bad response is used so often in the if tree, I made it into a function
+    //Not using the format::response function since the response itself changes depending on the request
+    let page = format!("{}\n{}", BAD_REQUEST_RESPONSE_RE_STR, buffer);
+    format!(
+        "{} {:?}\r\n\r\n{}",
+        BAD_REQUEST_HEADER_RE_STR,
+        page.len(),
+        page
     )
+}
+
+async fn request_regex_check (buffer: &String, request_type_re: &'static str) -> String {    
+    let (combined_path_re, valid_http_request_header_re) = {
+        if request_type_re == GET_RE {
+            (&*GET_COMBINED_PATH_RE, &*GET_VALID_HTTP_REQUEST_HEADER_RE)
+        } else {
+            (&*POST_COMBINED_PATH_RE, &*POST_VALID_HTTP_REQUEST_HEADER_RE)
+        }
+    };
+    
+    let index: usize = 2;
+
+    if combined_path_re.is_match(&buffer) && valid_http_request_header_re.is_match(&buffer) {
+        format_response(
+            combined_path_re.captures(&buffer).unwrap().get(index).map_or("", |m| m.as_str()),
+            200,
+        ).await.unwrap()
+    } else {
+        bad_request_page(&buffer).await
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,95 +3,32 @@
 use tokio::net::{TcpStream, TcpListener};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::spawn;
-use std::time::Duration;
-use std::thread::sleep;
-use reinda::{assets, Assets, Config, Setup};
-
-const ASSETS: Setup = assets! {
-    // Folder which contains your assets, relative to your `Cargo.toml`.
-    #![base_path = "Web"]
-
-    // List of assets to include, with different settings.
-    "index.html": { template },
-};
-
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:7878").await?;
-    /*
-    listener
-        .incoming()
-        .for_each_concurrent(/* limit */ None, |tcpstream| async move {
-            let tcpstream = tcpstream.unwrap();
-            handle_connection(tcpstream).await.unwrap();
-        })
-        .await;*/
-        
-    loop {
-        let (mut tcp_stream, _) = listener.accept().await.unwrap();
-        spawn(async move {
-           handle_connection(tcp_stream).await;
-        });
-    }
-        
-        Ok(())
-}
-
-async fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).await?;
-
-    let status_line = "HTTP/1.1 200 OK\r\n\r\n";
-
-    sleep(Duration::from_secs(10));
-    
-    let assets = Assets::new(ASSETS, Config::default()).await?;
-    let content_bytes = &assets.get("index.html").await?.unwrap();
-    let content = String::from_utf8_lossy(content_bytes);
-
-    let response = format!("{}{}", status_line, content);
-    stream.write(response.as_bytes()).await?;
-    stream.flush().await?;
-    
-    Ok(())
-}
-
-/*use std::io::prelude::{Write, Read};
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::fs::read_to_string;
-use threadpool::ThreadPool;
 use format::validate_request;
 
 mod format;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(100);
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:7878").await?;
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        pool.execute(|| {
-            handle_connection(stream);
+    loop {
+        let (tcp_stream, _) = listener.accept().await?;
+        spawn(async move {
+           handle_connection(tcp_stream).await.unwrap();
+           
         });
     }
+        
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
     let mut buffer: [u8; 1024] = [0; 1024];
+    stream.read(&mut buffer).await?;
     
-    stream.read(&mut buffer).unwrap();
-    let buffer = String::from_utf8_lossy(&buffer[..]);
-
-    let (response_header, path, post_data, get_data) = validate_request(buffer.to_string());
+    let buffer = String::from_utf8_lossy(&buffer).to_string();
+    let (response, path, post_data, get_data) = validate_request(buffer).await;
     
-    
-    let response = response_header;
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-
-
-}*/
+    stream.write(response.as_bytes()).await?;
+    stream.flush().await?;
+    Ok(())
+}
