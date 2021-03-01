3bf92d9cb888e92d7b58dbac892791d7cbe45312
William Batista <bootlegbilly@protonmail.ch>
2021-03-01 11:57:34 -0500

    Added authentication tokens
    When a user logs in, the client  is given an authentication token (since
    that can be safely stored on a user device) that will last for a month
    before expiring.

--- src/auth.rs
+++ src/auth.rs
@@ -1,11 +1,19 @@
+#![deny(clippy::all)]
+
 use tokio::task;
-use argon2::{self, Config};
-use rand::prelude::*;
+use rand::Rng;
+use rand::rngs::{ThreadRng as SaltRng};
 use warp::http::StatusCode;
 use std::collections::HashMap;
 use serde::Deserialize;
 use std::sync::Arc;
 use tokio::sync::Mutex;
+use sha2::{Sha256, Digest};
+use std::time::{SystemTime, UNIX_EPOCH};
+use argon2::{
+    password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2
+};
 
 #[derive(Deserialize)]
 pub struct User {
@@ -14,15 +22,22 @@ pub struct User {
     email: String,
 }
 
-pub async fn register(new_user: User, db: Arc<Mutex<HashMap<String, User>>>) -> Result<impl warp::Reply, warp::Rejection> {
-    let mut users = db.lock().await;
+#[derive(Deserialize)]
+struct AuthUser {
+    cookie: String,
+    username: String,
+}
+
+pub async fn register(new_user: User, cred_db: Arc<Mutex<HashMap<String, User>>>) -> Result<impl warp::Reply, warp::Rejection> {
+    let mut users = cred_db.lock().await;
         
     if users.contains_key(&new_user.username) {
-        return Ok(
-        warp::reply::with_status(
-            "You've already signed up!",
-            StatusCode::BAD_REQUEST,
-        )
+        return 
+            Ok(
+            warp::reply::with_status(
+                "You've already signed up!",
+                StatusCode::BAD_REQUEST,
+            )
         );
     }
     
@@ -39,8 +54,8 @@ pub async fn register(new_user: User, db: Arc<Mutex<HashMap<String, User>>>) -> 
         ))
 }
 
-pub async fn login(auth_user: User, db: Arc<Mutex<HashMap<String, User>>> ) -> Result<impl warp::Reply, warp::Rejection> {
-    let users = db.lock().await;
+pub async fn login(auth_user: User, cred_db: Arc<Mutex<HashMap<String, User>>>, auth_token_db: Arc<Mutex<HashMap<String, (String, u64)>>>) -> Result<impl warp::Reply, warp::Rejection> {
+    let users = cred_db.lock().await;
         
     match users.get(&auth_user.username) {
         None => Ok(warp::reply::with_status(
@@ -49,8 +64,25 @@ pub async fn login(auth_user: User, db: Arc<Mutex<HashMap<String, User>>> ) -> R
                             )),
         Some(user) => {
             if verify((&*user.password).to_string(), auth_user.password).await {
+                let mut hasher = Sha256::new(); 
+                // Get a sha256 hash of a random 16 byte long byte array
+                hasher.update(rand::thread_rng().gen::<[u8; 16]>());
+            
+                let mut auth_token_db = auth_token_db.lock().await;
+                
+                let auth_token = &hasher.finalize()[..];
+                let auth_token = hex::encode(auth_token);
+                
+                //After a month, a cookie should be considered invalid
+                let expiration_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() + 2629800;
+                
+                auth_token_db.insert((&*auth_token).to_string(), (auth_user.username, expiration_time));
+                
+                //Fuck the rust borrow checker, all my homies hate the Rust borrow checker
+                //Literally the only way I can convert the cookie String into an &'static str so warp login
+                // Can do its fucking job
                 Ok(warp::reply::with_status(
-                        "Logged in sucessfully!",
+                        Box::leak(auth_token.into_boxed_str()),
                         StatusCode::OK,
                 ))
             
@@ -65,22 +97,29 @@ pub async fn login(auth_user: User, db: Arc<Mutex<HashMap<String, User>>> ) -> R
     }
 }
 
-async fn verify(passwd: String, auth_passwd: String) -> bool {
+async fn verify(password_hash: String, auth_passwd: String) -> bool {
     // Run in tokio's spawn_blocking for efficiency
     task::spawn_blocking(move || {
-        argon2::verify_encoded(&passwd, auth_passwd.as_bytes()).unwrap()
+        let argon2 = Argon2::default();
+    
+       let parsed_hash = PasswordHash::new(&password_hash).unwrap();
+       
+       argon2.verify_password(auth_passwd.as_bytes(), &parsed_hash).is_ok()
         
     }).await.unwrap()    
 }
 
 
-async fn hash(passwd: String) -> String {
+async fn hash(password: String) -> String {
     // Run in tokio's spawn_blocking for efficiency
     task::spawn_blocking(move || {
-        let salt = rand::thread_rng().gen::<[u8; 16]>();
+        let salt = SaltString::generate(&mut SaltRng::default());
+        let argon2 = Argon2::default();
+        
         println!("{:?}", &salt);
+        
+        argon2.hash_password_simple(password.as_bytes(), salt.as_ref()).unwrap().to_string()
     
-        argon2::hash_encoded(passwd.as_bytes(), &salt, &Config::default()).unwrap()
         
     }).await.unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![deny(clippy::all)]
+
 use warp::Filter;
 use tokio::runtime::Builder;
 use std::sync::Arc;
@@ -12,19 +14,24 @@ rt.block_on(async {
 
     // The big ol' database storing user data n stuff
     // The key is the username, and the val is said user's User struct
-    let db = Arc::new(Mutex::new(HashMap::<String, User>::new()));
-    let db = warp::any().map(move || Arc::clone(&db));
+    let cred_db = Arc::new(Mutex::new(HashMap::<String, User>::new()));
+    let cred_db = warp::any().map(move || Arc::clone(&cred_db));
+    
+    //Currently logged in users and their session cookies
+    let cookie_db = Arc::new(Mutex::new(HashMap::<String, (String, u64)>::new()));
+    let cookie_db = warp::any().map(move || Arc::clone(&cookie_db));
     
     let register = warp::post()
         .and(warp::path("register"))
         .and(warp::body::form())
-        .and(db.clone())
+        .and(cred_db.clone())
         .and_then(register);
     
     let login = warp::post()
         .and(warp::path("login"))
         .and(warp::body::form())
-        .and(db.clone())
+        .and(cred_db.clone())
+        .and(cookie_db.clone())
         .and_then(login);
     
     let routes = warp::post().and(
